8703c8b74a2057a21a19a3de57685aa5c39d2cc0
charleshug <charleshug@gmail.com>
2023-02-03 15:09:19 -0800

    added navbar hamburger for mobile devices

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -8,4 +8,11 @@ document.addEventListener("turbo:load", function() {
     let menu = document.querySelector("#dropdown-menu");
     menu.classList.toggle("active");
   });
+
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", function(event){
+    event.preventDefault();
+    let menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
+  });
 });
