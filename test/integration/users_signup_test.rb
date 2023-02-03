45731d4d00bd1bdf977df69df69bbdb29ebc9805
charleshug <charleshug@gmail.com>
2023-02-03 12:25:13 -0800

    integration test for successful user create

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,11 +8,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          email: "user@invalid",
                                          password: "foo",
                                          password_confirmation: "bar" }
-    }
+      }
     end
     assert_response :unprocessable_entity
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
