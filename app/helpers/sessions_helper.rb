291aef8147023596c8b2f20a2576d30f1eebf670
charleshug <charleshug@gmail.com>
2023-02-04 09:13:03 -0800

    add remember user to sessions controller upon session create

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,17 +3,23 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     end
   end
-
+  
   def logged_in?
     !current_user.nil?
   end
-
+  
   def log_out
     reset_session
     @current_user = nil
